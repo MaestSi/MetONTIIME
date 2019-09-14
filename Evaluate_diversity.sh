796dc6475eb83d3e39025b55117082782f2dea40
Simone Maestri <simone.maestri@hotmail.it>
2019-09-15 01:56:08 +0200

    Subsampling is performed before building the tree

--- Evaluate_diversity.sh
+++ Evaluate_diversity.sh
@@ -18,7 +18,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-usage="$(basename "$0") [-w working_directory] [-m sample_metadata] [-d sampling_depth] [-t threads]"
+usage="$(basename "$0") [-w WORKING_DIRECTORY] [-m SAMPLE_METADATA] [-d SAMPLING_DEPTH] [-t THREADS]"
 
 while :
 do
@@ -28,24 +28,24 @@ do
           exit 0
           ;;
       -w)
-          working_directory=$(realpath $2)
+          WORKING_DIRECTORY=$(realpath $2)
           shift 2
-          echo "Working directory: $working_directory"
+          echo "Working directory: $WORKING_DIRECTORY"
           ;;
       -m)
-           sample_metadata=$(realpath $2)
+           SAMPLE_METADATA=$(realpath $2)
            shift 2
-           echo "Sample metadata: $sample_metadata"
+           echo "Sample metadata: $SAMPLE_METADATA"
            ;;
       -d)
-           sampling_depth=$2
+           SAMPLING_DEPTH=$2
            shift 2
-           echo "Minimum sampling depth: $sampling_depth reads"
+           echo "Sampling depth: $SAMPLING_DEPTH reads"
            ;;
       -t)
-           threads=$2
+           THREADS=$2
            shift 2
-           echo "Number of threads: $threads"
+           echo "Number of THREADS: $THREADS"
            ;;
        --) # End of all options
            shift
@@ -65,25 +65,60 @@ done
 
 source activate MetONTIIME_env
 
+FASTQ_FILES=$(realpath $(find $WORKING_DIRECTORY | grep "\.fastq\.gz" | grep -v "subsampled"))
+
+MANIFEST_SUB=$WORKING_DIRECTORY"/manifest_"$SAMPLING_DEPTH"_subsampled.txt"
+
+if [ ! -f "$MANIFEST_SUB" ]; then
+  echo -e sample-id"\t"absolute-filepath > $MANIFEST_SUB
+  for f in $FASTQ_FILES; do
+    s=$(echo $(basename $f) | sed 's/\.fastq\.gz//g')
+    s_sub=$s"_"$SAMPLING_DEPTH"_subsampled"
+    f_sub=$WORKING_DIRECTORY"/"$s_sub".fastq.gz"
+    seqtk sample $f $SAMPLING_DEPTH | gzip > $f_sub
+    echo -e $s"\t"$f_sub >> $MANIFEST_SUB;
+  done
+fi
+
+qiime tools import \
+--type 'SampleData[SequencesWithQuality]' \
+--input-path $MANIFEST_SUB \
+--input-format 'SingleEndFastqManifestPhred33V2' \
+--output-path "sequences_"$SAMPLING_DEPTH"_subsampled.qza"
+
+qiime vsearch dereplicate-sequences \
+--i-sequences "sequences_"$SAMPLING_DEPTH"_subsampled.qza" \
+--o-dereplicated-table "table_tmp_"$SAMPLING_DEPTH"_subsampled.qza" \
+--o-dereplicated-sequences "rep-seqs_tmp_"$SAMPLING_DEPTH"_subsampled.qza"
+
+qiime vsearch cluster-features-de-novo \
+--i-sequences "rep-seqs_tmp_"$SAMPLING_DEPTH"_subsampled.qza" \
+--i-table "table_tmp_"$SAMPLING_DEPTH"_subsampled.qza" \
+--p-perc-identity 1 \
+--o-clustered-table "table_"$SAMPLING_DEPTH"_subsampled.qza" \
+--o-clustered-sequences "rep-seqs_"$SAMPLING_DEPTH"_subsampled.qza" \
+--p-threads $THREADS
+
+rm "table_tmp_"$SAMPLING_DEPTH"_subsampled.qza" "rep-seqs_tmp_"$SAMPLING_DEPTH"_subsampled.qza"
+
 qiime phylogeny align-to-tree-mafft-fasttree \
-  --i-sequences $working_directory/rep-seqs.qza \
-  --o-alignment $working_directory/aligned-rep-seqs.qza \
-  --o-masked-alignment $working_directory/masked-aligned-rep-seqs.qza \
-  --o-tree $working_directory/unrooted-tree.qza \
-  --o-rooted-tree $working_directory/rooted-tree.qza \
-  --p-n-threads $threads
+  --i-sequences $WORKING_DIRECTORY"/rep-seqs_"$SAMPLING_DEPTH"_subsampled.qza" \
+  --o-alignment $WORKING_DIRECTORY"/aligned-rep-seqs_"$SAMPLING_DEPTH"_subsampled.qza" \
+  --o-masked-alignment $WORKING_DIRECTORY"/masked-aligned-rep-seqs_"$SAMPLING_DEPTH"_subsampled.qza" \
+  --o-tree $WORKING_DIRECTORY"/unrooted-tree_"$SAMPLING_DEPTH"_subsampled.qza" \
+  --o-rooted-tree $WORKING_DIRECTORY"/rooted-tree_"$SAMPLING_DEPTH"_subsampled.qza" \
+  --p-n-threads $THREADS
 
 qiime diversity core-metrics-phylogenetic \
-  --i-phylogeny $working_directory/rooted-tree.qza \
-  --i-table $working_directory/table.qza \
-  --p-sampling-depth $sampling_depth \
-  --m-metadata-file $sample_metadata \
-  --output-dir $working_directory/core-metrics-results \
+  --i-phylogeny $WORKING_DIRECTORY"/rooted-tree_"$SAMPLING_DEPTH"_subsampled.qza" \
+  --i-table $WORKING_DIRECTORY"/table_"$SAMPLING_DEPTH"_subsampled.qza" \
+  --p-sampling-depth $SAMPLING_DEPTH \
+  --m-metadata-file $SAMPLE_METADATA \
+  --output-dir $WORKING_DIRECTORY"/core-metrics-results_"$SAMPLING_DEPTH"_subsampled" \
 
 qiime diversity alpha-rarefaction \
-  --i-table $working_directory/table.qza \
-  --i-phylogeny $working_directory/rooted-tree.qza \
-  --p-max-depth $sampling_depth \
-  --m-metadata-file $sample_metadata \
-  --o-visualization $working_directory/alpha-rarefaction.qzv
-
+  --i-table $WORKING_DIRECTORY"/table_"$SAMPLING_DEPTH"_subsampled.qza" \
+  --i-phylogeny $WORKING_DIRECTORY"/rooted-tree_"$SAMPLING_DEPTH"_subsampled.qza" \
+  --p-max-depth $SAMPLING_DEPTH \
+  --m-metadata-file $SAMPLE_METADATA \
+  --o-visualization $WORKING_DIRECTORY"/alpha-rarefaction_"$SAMPLING_DEPTH"_subsampled.qzv"
