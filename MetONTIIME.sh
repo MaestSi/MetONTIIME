c69aa3f982412b8f8212d9e56596e6e1265fced3
Simone Maestri <simone.maestri@hotmail.it>
2019-10-25 16:33:07 +0200

    Update MetONTIIME.sh

--- MetONTIIME.sh
+++ MetONTIIME.sh
@@ -166,7 +166,7 @@ biom convert \
 
 #NCBI database
 cat feature-table_absfreq.tsv | grep "#" > header
-cat feature-table_absfreq.tsv | grep -v -P "Unassigned|#|BC" | cut -f6 -d';' | grep -v "__" | sort -k3,3 -nr > species_counts_noheader.txt
+cat feature-table_absfreq.tsv | grep -v -P "Unassigned|#|BC|NA" | cut -f6 -d';' | tr "\t" "," | grep -v "__" | sort -t"," -k2,2 -nr | tr "," "\t" > species_counts_noheader.txt
 
 cat header species_counts_noheader.txt > species_counts.txt
 
